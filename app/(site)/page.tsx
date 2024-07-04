3160898c7d36870949ab13378d51300d6610e1f7
dev-billah <dev.billah@gmail.com>
2024-07-04 12:41:48 +0600

    added navbar

---
+++ app/(site)/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/shared/Navbar";
+
+export default function Home() {
+  return (
+    <main className="bg-gradient-to-b from-blue-100 via-white to-blue-100">
+      <div className="max-w-7xl mx-auto">
+        <Navbar />
+      </div>
+    </main>
+  );
+}
